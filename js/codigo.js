ad48001482143a4f4b4077687a762805247ce215
GusGusGusta <1993gustavo1993@gmail.com>
2024-03-26 21:08:13 -0300

    1

---
+++ js/codigo.js
@@ -0,0 +1,12 @@
+//linea
+/*parrafo*/
+
+
+alert("Segundo mensaje de ejemplo por hoja js");
+
+alert("tercer mensaje de ejemplo por hoja \n \n  Querido Curso! \n \t Que facil es incluir \' comillas simples\' \n y \"comillas dobles \" \n ademas de salto de linea :D ");
+var meses = "L"
+var meses = ["Enero", "Febrero", "Marzo", "Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
+
+alert("Primer Semestre  : " + "\n" + meses[0] + "\n" + meses[1] +"\n"+ meses[2]+"\n \n "+"Segundo Semestre :" + "\n " + meses[7]);
+
